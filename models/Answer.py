e980f63445ad2c62abb8ee09887e5677cb75cda7
s4mkoff <vadym.samkov@trident-media.agency>
2025-07-19 22:29:27 +0300

    New images

--- models/Answer.py
+++ models/Answer.py
@@ -1,10 +1,16 @@
+from enum import Enum
+from typing import Dict
+
+class KiloType(Enum):
+    K_1300 = 1300
+    K_1500 = 1500
+    K_1700 = 1700
+
 class Answer:
     def __init__(
         self,
         imageSrc: str,
-        text: str,
-        buttons: list
+        text: Dict[KiloType, str]
     ):
         self.imageSrc = imageSrc
         self.text = text
-        self.buttons = buttons
